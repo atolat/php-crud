6fcf4e21f533075015cb887efa1d92c114637a56
Arpan Tolat <Arpan Tolat>
2016-06-29 16:42:13 -0500

    Added string escapes and password encryption

--- functions.php
+++ functions.php
@@ -35,6 +35,12 @@
         if(isset($_POST['submit'])){
             $username = $_POST['username'];
             $password = $_POST['password'];
+            $username = mysqli_real_escape_string($connection,$username);
+            $password = mysqli_real_escape_string($connection,$password);
+            $hashFormat = "$2y$10$";
+            $salt = "iusesomecrazystrings22";
+            $hashFormat_and_salt = $hashFormat.$salt;
+            $password = crypt($password,$hashFormat_and_salt);
             $query = "INSERT INTO users(username,password) VALUES ('$username','$password')";
             $result = mysqli_query($connection,$query);
         if(!$result){
